787a76f4b73ba6ecd3fe47d77902f298b38fa1a7
Jonathan Belelieu <jonnyobell@gmail.com>
2022-04-27 16:11:49 -0400

    TItle fix and updated screen shot

--- templates/tax/estimate.php
+++ templates/tax/estimate.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title><?php echo $project['title'] . ": " . $collection['title']; ?></title>
+    <title>Tax Estimate: <?php echo $displayType; ?> (ATOS)</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style type="text/css">
         <?php echo $css; ?>
